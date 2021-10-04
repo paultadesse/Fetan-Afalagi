0df7d7e34253727be99cbec73a1b92c30f75bf7f
Paulos Tadesse <paulosta952@gmail.com>
2021-10-04 09:50:39 +0300

    :zap: migration, controller, model, factory and seeders created for both category and info table

---
+++ database/migrations/2021_10_04_063502_create_infos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('addrerss');
+            $table->string('pobox');
+            $table->string('fax');
+            $table->string('email');
+            $table->text('about');
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('infos');
+    }
+}
